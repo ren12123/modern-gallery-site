b0f722b40861cccc065e7eb04852726a04e87b6f
s1f102401012 <s1f102401012@iniad.org>
2026-03-20 15:30:56 +0900

    add gallery

---
+++ style.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // 1. 動かしたい要素を取得
+    const leftImg = document.querySelector('.main-gallery__img--left');
+    const rightImg = document.querySelector('.main-gallery__img--right');
+    const allImg = document.querySelectorAll('.main-gallery__img');
+
+    let ticking = false;
+
+    // 2. スクロールイベントの登録
+    window.addEventListener('scroll', () => {
+        // パフォーマンス対策：描画のタイミング（1秒間に60回）に合わせて計算を実行
+        if (!ticking) {
+            window.requestAnimationFrame(() => {
+                updateAnimation();
+                ticking = false;
+            });
+            ticking = true;
+        }
+    }, { passive: true }); // スクロールを滑らかにするためのオプション
+
+    // 3. 実際の計算・アニメーション処理
+    function updateAnimation() {
+        const scrollY = window.scrollY; // 現在のスクロール量（px）を取得
+
+        // 768px以上の時だけ画像をスライドさせる
+        if (window.innerWidth > 768) {
+            // 例：スクロール量に合わせて最大20pxまで左右に広げる
+            const moveAmount = Math.min(scrollY * 1.8, 350);
+            const scaleAmount = Math.min(1 + scrollY * 0.003, 3);
+
+            if (allImg.length > 0) {
+                allImg.forEach((img) => {
+                    if (img.classList.contains('main-gallery__img--left')) {
+                        img.style.transform = `translateX(-${moveAmount}px) scale(${scaleAmount})`;
+                    }
+
+                    else if (img.classList.contains('main-gallery__img--right')) {
+                        img.style.transform = `translateX(${moveAmount}px) scale(${scaleAmount})`;
+                    } 
+                    // 真ん中の画像の場合
+                    else {
+                        img.style.transform = `scale(${scaleAmount})`;
+                    }
+                });
+            }
+        }
+    }
+});
